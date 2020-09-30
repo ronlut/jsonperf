96a287213165a4ae459df4278033532622b2da76
Rony Lutsky <ronlut@gmail.com>
2020-09-30 16:36:16 +0300

    Add social metadata tags

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,16 @@
 module.exports = {
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+          args[0].title = "JSONPerf";
+          return args;
+        })
+  },
   transpileDependencies: ["vuetify", "vue-echarts", "resize-detector"],
   configureWebpack: {
     devServer: {
+      // disableHostCheck: true,
       proxy: {
         "/": {
           target: "http://localhost:5000",
