e6f07f91e69465b51c84c5a7ff7fca1f03982def
Rony Lutsky <ronlut@gmail.com>
2020-10-29 19:31:51 +0200

    add google analytics

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,15 @@ import store from "./store";
 import ECharts from "vue-echarts";
 import "echarts/lib/chart/bar";
 import vuetify from "./plugins/vuetify";
+import VueGtag from "vue-gtag";
 
 Vue.config.productionTip = false;
 Vue.component("v-chart", ECharts);
+Vue.use(VueGtag, {
+  config: {
+    id: "G-2FB2J1LW8R"
+  }
+});
 
 new Vue({
   router,
